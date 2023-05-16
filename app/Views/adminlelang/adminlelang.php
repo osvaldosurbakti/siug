241882133e0c82a3b66e5fb2bc9e97b817826cda
osvaldosurbakti <osvaldosurbakti@gmail.com>
2023-05-17 01:58:32 +0700

    fungsional hapus berhasil

--- app/Views/adminlelang/adminlelang.php
+++ app/Views/adminlelang/adminlelang.php
@@ -204,9 +204,13 @@
                                                 </td>
                                                 <td><?= $bl['kelengkapan_barang']; ?></td>
                                                 <td>
-                                                    <a href="#" class="btn btn-danger btn-circle btn-sm">
-                                                        <i class="fas fa-trash"></i>
-                                                    </a>
+                                                    <form action="/adminlelang/delete/<?= $bl['id']; ?>" method="post" class="d-inline">
+                                                        <?= csrf_field(); ?>
+                                                        <input type="hidden" name="_method" value="DELETE">
+                                                        <button type="submit" class="btn btn-danger btn-circle btn-sm" onclick="return confirm('Apakah anda yakin dihapus?')">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </form>
                                                     <a href="/adminlelang/<?= $bl['id']; ?>" class="btn btn-info btn-circle btn-sm">
                                                         <i class="fas fa-info-circle"></i>
                                                     </a>
