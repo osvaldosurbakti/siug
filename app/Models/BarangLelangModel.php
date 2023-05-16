a716d82361c8bf01fecb0d7796f6a6f419305ed5
osvaldosurbakti <osvaldosurbakti@gmail.com>
2023-05-16 10:43:33 +0700

    selesaikonektapiviewerror

--- app/Models/BarangLelangModel.php
+++ app/Models/BarangLelangModel.php
@@ -8,5 +8,12 @@ class BarangLelangModel extends Model
 {
     protected $table = 'barang_lelang';
     protected $useTimestamps = true;
-    protected $allowedFields = ['nama_barang', 'harga_barang', 'gambar_barang', 'kelengkapan_barang'];
+
+    public function getBarangLelang($id = false)
+    {
+        if ($id == false) {
+            return $this->findAll();
+        }
+        return $this->where(['id' => $id])->first();
+    }
 }
